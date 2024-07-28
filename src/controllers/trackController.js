d9aa7d70ac208806cc3f18de1340fdc48d7ac38f
Farid Virani <fvirani786@gmail.com>
2024-07-28 18:04:31 -0500

    feat:commit

--- src/controllers/trackController.js
+++ src/controllers/trackController.js
@@ -4,7 +4,7 @@ const Track = require('../models/track');
 exports.createTrack = async (req, res) => {
   try {
     const newTrack = new Track(req.body);
-    await newTrack.save();
+    await newTracks.save();
     res.status(201).json(newTrack);
   } catch (err) {
     res.status(500).json({ message: err.message });
